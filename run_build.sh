cc28286c2973aff026ffd607e7ef0baf29a69bd2
Bastian Schumacher <Bastian.Schumacher@gmx.de>
2017-04-14 17:38:05 +0200

    big cleanup

---
+++ run_build.sh
@@ -0,0 +1,69 @@
+#! /bin/bash
+
+NC='\033[0m'     # No Color
+EC='\033[0;31m'  # Red
+SC='\033[0;32m'  # Green
+WC='\033[0;33m'  # Orange
+IC='\033[0;34m'  # Blue
+
+info() { echo -e "${IC}$1${NC}"; }
+warn() { echo -e "${WC}Warning:${NC} $1"; }
+error() { >&2 echo -e "${EC}Error: $1${NC}"; }
+success() { echo -e "${SC}$1${NC}"; }
+
+if [ ! $SOURCE_DIR ] ; then
+  warn "\$SOURCE_DIR undefined! Setting it to: ${PWD}"
+  SOURCE_DIR=${PWD}
+fi
+
+if [ ! $BUILD_DIR ] ; then
+  warn "\$BUILD_DIR undefined! Setting it to: ${SOURCE_DIR}/build"
+  BUILD_DIR=${SOURCE_DIR}/build
+fi
+
+info "Setting up build directories"
+mkdir -p ${BUILD_DIR}
+cd ${BUILD_DIR}
+mkdir -p gcc/release && mkdir -p gcc/debug
+if [ $? -ne 0 ]; then
+    error "Could not setup build directories!"
+    exit 1
+else
+  success "Successfully generated build directories."
+fi
+
+info "Building GCC Release"
+cd ${BUILD_DIR}/gcc
+cmake -DCMAKE_CXX_COMPILER=g++ -DCMAKE_C_COMPILER=gcc \
+  -DCMAKE_BUILD_TYPE=Release \
+  -G Ninja -DCMAKE_MAKE_PROGRAM=ninja \
+  -DBUILD_TESTING=ON \
+  ${SOURCE_DIR}
+ninja -j8
+if [ $? -ne 0 ]; then
+    error "There are compile errors!"
+    exit 2
+else
+  success "Successfully built GCC Release"
+fi
+
+#info "Running tests on GCC Release build"
+#src/library/pluginManager/pluginManagerTest
+
+info "Building GCC Debug"
+cd ${BUILD_DIR}/gcc/debug
+cmake -DCMAKE_CXX_COMPILER=g++ -DCMAKE_C_COMPILER=gcc \
+  -DCMAKE_BUILD_TYPE=Debug \
+  -G Ninja -DCMAKE_MAKE_PROGRAM=ninja  \
+  -DBUILD_TESTING=ON -DGENERATE_COVERAGE=ON \
+  ${SOURCE_DIR}
+ninja -j8
+if [ $? -ne 0 ]; then
+    error "There are compile errors!"
+    exit 3
+else
+  success "Successfully built GCC Debug"
+fi
+#info "Running tests on GCC Debug build"
+#ninja test
+
--- test/library/pluginManager/pluginmanagertest.cpp
+++ test/library/pluginManager/pluginmanagertest.cpp
@@ -1,7 +1,5 @@
 #include "pluginmanagertest.hpp"
 
-#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main()
-                           // TODO: - only do this in one cpp file
 #include "catch.hpp"
 
 #include "pluginmanager.hpp"
