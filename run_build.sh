7ba80d3ebc960de18b3448ef9199b7d4aa5fa52e
Bastian Schumacher <Bastian.Schumacher@gmx.de>
2017-05-03 13:53:26 +0200

    replaced kcov with lcov

--- run_build.sh
+++ run_build.sh
@@ -11,6 +11,16 @@ warn() { echo -e "${WC}Warning:${NC} $1"; }
 error() { >&2 echo -e "${EC}Error: $1${NC}"; }
 success() { echo -e "${SC}$1${NC}"; }
 
+if [ ! $CC ] ; then
+  warn "\$CC undefined! Setting it to: gcc"
+  CC=gcc
+fi
+
+if [ ! $CXX ] ; then
+  warn "\$CXX undefined! Setting it to: g++"
+  CXX=g++
+fi
+
 if [ ! $SOURCE_DIR ] ; then
   warn "\$SOURCE_DIR undefined! Setting it to: ${PWD}"
   SOURCE_DIR=${PWD}
@@ -21,58 +31,85 @@ if [ ! $BUILD_DIR ] ; then
   BUILD_DIR=${SOURCE_DIR}/build
 fi
 
+if [ ! $RUN_TESTS ] ; then
+  warn "\$RUN_TESTS undefined! Setting it to: OFF"
+  RUN_TESTS=OFF
+fi
+
 info "Setting up build directories"
 mkdir -p ${BUILD_DIR}
 cd ${BUILD_DIR}
 mkdir -p gcc/release && mkdir -p gcc/debug
 if [ $? -ne 0 ]; then
-    error "Could not setup build directories!"
-    exit 1
+  error "Could not setup build directories!"
+  exit 1
 else
   success "Successfully generated build directories."
 fi
 
-info "Building GCC Release"
-cd ${BUILD_DIR}/gcc
-cmake -DCMAKE_CXX_COMPILER=g++ -DCMAKE_C_COMPILER=gcc \
-  -DCMAKE_BUILD_TYPE=Release \
+info "Building Release"
+cd ${BUILD_DIR}/${CC}/release
+cmake -DCMAKE_BUILD_TYPE=Release \
   -G Ninja -DCMAKE_MAKE_PROGRAM=ninja \
+  -DSODA_BUILD_TESTS=${RUN_TESTS} \
   ${SOURCE_DIR}
 ninja -j8
 if [ $? -ne 0 ]; then
-    error "There are compile errors!"
-    exit 2
+  error "There are compile errors!"
+  exit 2
 else
-  success "Successfully built GCC Release"
+  success "Successfully built Release"
 fi
 
-info "Building GCC Debug"
-cd ${BUILD_DIR}/gcc/debug
-cmake -DCMAKE_CXX_COMPILER=g++ -DCMAKE_C_COMPILER=gcc \
-  -DCMAKE_BUILD_TYPE=Debug \
+info "Building Debug"
+cd ${BUILD_DIR}/${CC}/debug
+cmake -DCMAKE_BUILD_TYPE=Debug \
   -G Ninja -DCMAKE_MAKE_PROGRAM=ninja  \
-  -DSODA_BUILD_TESTS=ON \
+  -DSODA_BUILD_TESTS=OFF \
   ${SOURCE_DIR}
 ninja -j8
 if [ $? -ne 0 ]; then
-    error "There are compile errors!"
-    exit 3
+  error "There are compile errors!"
+  exit 3
 else
   success "Successfully built GCC Debug"
 fi
 
 cd ${SOURCE_DIR}
 if [ ${GENERATE_COVERAGE} ]; then
-    info "Running tests and Generate coverage"
-    kcov --exclude-path=/usr/ --exclude-pattern=moc_ . bin/tests
-    if [ ${UPLOAD_COVERAGE} ]; then
-        info "Uploading report to CodeCov"
-        bash <(curl -s https://codecov.io/bash) || error "Codecov did not collect coverage reports"
-    fi
-else 
-    if [ ${RUN_TESTS} ] ; then
-        info "Running tests on GCC Debug build"
-        ./bin/tests
-    fi
-fi
+  info "Building GCC Release with debug info and --coverage"
+  mkdir -p ${BUILD_DIR}/gcc/coverage
+  cd ${BUILD_DIR}/gcc/coverage
+  cmake  -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++\
+    -DCMAKE_CXX_FLAGS=--coverage \
+    -DCMAKE_BUILD_TYPE=RelWithDebInfo \
+    -G Ninja -DCMAKE_MAKE_PROGRAM=ninja \
+    -DSODA_BUILD_TESTS=ON -DSODA_GENERATE_COVERAGE=ON\
+    ${SOURCE_DIR}
+  ninja -j8 TestCoverage
+  if [ $? -ne 0 ]; then
+    error "There are compile errors!"
+    exit 2
+  else
+    success "Successfully built clang Release for Test coverage"
+  fi
 
+  info "Running tests and Generate coverage"
+  # capture coverage info
+  lcov --directory . --capture --output-file coverage.info
+  # filter result
+  lcov --remove coverage.info '/usr/*' --output-file coverage.info
+  lcov --remove coverage.info 'moc_*' --output-file coverage.info
+  lcov --remove coverage.info 'test/' --output-file coverage.info
+  # display results
+  lcov --list coverage.info
+  if [ ${UPLOAD_COVERAGE} ]; then
+    info "Uploading report to CodeCov"
+    bash <(curl -s https://codecov.io/bash) || error "Codecov did not collect coverage reports"
+  fi
+else 
+  if [ ${RUN_TESTS} ] ; then
+    info "Running tests on GCC Debug build"
+    ${SOURCE_DIR}/bin/tests
+  fi
+fi
